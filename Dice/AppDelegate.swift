a2caddecdf748d06b7979baa55690650f3bba794
Stephen Skubik-Peplaski <codesmith@me.com>
2015-09-23 11:58:02 -0400

    Initial Commit

---
+++ Dice/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Dice
+//
+//  Created by Stephen Skubik-Peplaski on 9/23/15.
+//  Copyright © 2015 Stephen Skubik-Peplaski. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
