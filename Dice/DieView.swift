43732c4d84f1d0f18bbb752bf3c51adadfe50e69
Stephen Skubik-Peplaski <codesmith@me.com>
2015-09-23 12:06:32 -0400

    Custom view for drawing in place

---
+++ Dice/DieView.swift
@@ -0,0 +1,13 @@
+//
+//  DieView.swift
+//  Dice
+//
+//  Created by Stephen Skubik-Peplaski on 9/23/15.
+//  Copyright © 2015 Stephen Skubik-Peplaski. All rights reserved.
+//
+
+import Cocoa
+
+class DieView: NSView {
+    
+}
